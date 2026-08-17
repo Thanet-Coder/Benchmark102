5641b41ea41ba33a172d1291cce6e01c132c349c
David Williams <51944525+Thanet-Coder@users.noreply.github.com>
2026-08-17 18:47:45 +0100

    Add Python environment information

--- Benchmark102.py
+++ Benchmark102.py
@@ -1,1 +1,14 @@
+import sys
+import platform
+
+print("LightCryptBench - Benchmark102")
+print()
+print("Python version:", sys.version)
+print("Python executable:", sys.executable)
+print("Operating system:", platform.system())
+print("OS version:", platform.version())
+print("Machine:", platform.machine())
+print("Processor:", platform.processor())
+
+
 
